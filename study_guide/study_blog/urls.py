dbd6ac9cd09abd5bd4bb49fd6f07ebb4adba6fb7
Maurice Okumu <maurice@Maurices-MacBook-Pro.local>
2020-07-12 11:23:41 -0500

    Add posting functionality

--- study_guide/study_blog/urls.py
+++ study_guide/study_blog/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from . import views
+from .views import (
+    StudyListView, 
+    AboutView,
+    StudyDetailView, 
+    StudyCreateView
+)
+# from . import views
 
 urlpatterns = [
-    path('', views.home, name='study-home'),
-    path('about/', views.about, name='study-about'),
+    # path('', views.home, name='study-home'),
+    path('', StudyListView.as_view(), name='study-home'),
+    # path('about/', views.about, name='study-about'),
+    path('about/', AboutView.as_view(), name='study-about'),
+     path('study/new/', StudyCreateView.as_view(), name='study-create'),
+    path('study/<int:pk>/', StudyDetailView.as_view(), name='study-detail'),
 ]
--- study_guide/study_blog/views.py
+++ study_guide/study_blog/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView
+)
 from .models import Study
 
+# Function view
+# def home(request):
+#     context = {
+#         'studies': Study.objects.all()
+#     }
+#     return render(request,'study_blog/home.html', context)
 
-def home(request):
-    context = {
-        'studies': Study.objects.all()
-    }
-    return render(request,'study_blog/home.html', context)
 
 
+# def about(request):
+#     return render(request,'study_blog/about.html')
 
-def about(request):
-    return render(request,'study_blog/about.html')
+
+#Class based views
+class StudyListView(ListView):
+    model = Study
+    template_name = 'study_blog/home.html'
+    context_object_name = 'studies'
+    ordering = ['date_posted']
+
+
+class StudyDetailView(DetailView):
+    model = Study
+
+class StudyCreateView(CreateView):
+    model = Study
+    fields = ['title', 'content', 'author']
+
+class AboutView(ListView):
+    model = Study
+    template_name = 'study_blog/about.html'
